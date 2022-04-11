03d9d9ed0fa43b8188cc7ee9789aade869e7b032
Abideen <Abideenali926@gmail.com>
2022-04-11 04:44:45 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Navstyling from './components/Navstyling'
+
+function App() {
+  return (
+    <>
+      <Navstyling />
+    </>
+  );
+}
+
+export default App;
